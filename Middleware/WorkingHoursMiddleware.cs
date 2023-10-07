f9063fb7f4d9a313b48432739a531bb51bf1fbe1
Moxitech <moxitogame59@gmail.com>
2023-10-08 00:02:15 +0300

    Обновление middleware

--- Middleware/WorkingHoursMiddleware.cs
+++ Middleware/WorkingHoursMiddleware.cs
@@ -16,12 +16,12 @@
         {
             var currentTime = DateTime.Now.TimeOfDay;
 
-            //if (currentTime < new TimeSpan(8, 0, 0) || currentTime > new TimeSpan(20, 0, 0))
-            //{
-            //    context.Response.StatusCode = StatusCodes.Status403Forbidden;
-            //    await context.Response.WriteAsync("Магазин работает с 8:00 утра до 20:00 вечера");
-            //    return;
-            //}
+            if (currentTime < new TimeSpan(8, 0, 0) || currentTime > new TimeSpan(20, 0, 0))
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("Магазин работает с 8:00 утра до 20:00 вечера");
+                return;
+            }
 
             await _next(context);
         }
